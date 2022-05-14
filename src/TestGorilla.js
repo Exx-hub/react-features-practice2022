af5d93e2a047d03209149343fa1cd371dae37c51
Alvin Acosta <alvin.acosta@movon.com.ph>
2022-05-14 15:14:47 +0800

    revert

--- src/TestGorilla.js
+++ src/TestGorilla.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 function TestGorilla() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  console.log("hello");
+
+  useEffect(() => {
+    console.log("test Gorilla useeffect code");
+    setCount((c) => c + 1);
+  }, []);
+
+  return <div>{count}</div>;
 }
 
 export default TestGorilla;
